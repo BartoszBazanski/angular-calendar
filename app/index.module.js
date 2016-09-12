2267e30106d48d2f64d0976c52c690065e6e719f
Bartosz Bazański <bartosz.b@webinterpreter.com>
2016-09-12 14:26:06 +0200

    Add function that generates days in month and days in the whole first week with day from previous month

--- app/index.module.js
+++ app/index.module.js
@@ -1,36 +1,33 @@
 (function(){
-    var dayInMilisec = 86400000;
-    var app = angular.module('angularCalendar',[]);
-    app.controller('CalendarController', function(){
-        this.day = today;
-        this.dayInMilisec = dayInMilisec;
-        this.firstDayOfMonth = today.date - (firstDayOfMonth.getDate() - 1) * dayInMilisec;
-        this.months = months;
-    });
-    var arrayOfNumbers = function(number){
-        var array = [];
-        for(var i = 1; i <= number; i++){
-            array.push(i);
-        }
-        return array;
-    }
-    var today = {
-        date: Date.now(),
-        weekDay: 'Thursday'
-    }
-    var months = {
-        january: arrayOfNumbers(31),
-        february: arrayOfNumbers(29),
-        march: arrayOfNumbers(31),
-        april: arrayOfNumbers(30),
-        may: arrayOfNumbers(31),
-        june: arrayOfNumbers(30),
-        july: arrayOfNumbers(31),
-        august: arrayOfNumbers(31),
-        september: arrayOfNumbers(30),
-        october: arrayOfNumbers(31),
-        november: arrayOfNumbers(30),
-        december: arrayOfNumbers(31)
-    }
-    var firstDayOfMonth = new Date(today.date);
+    angular.module('angularCalendar',[])
+        .controller('CalendarController', function(){
+            var calendar = this;
+            calendar.day = new Date(2015, 6, 14);
+            calendar.firstDayOfMonth = new Date(calendar.day.getFullYear(), calendar.day.getMonth(), 1);
+            calendar.lastDayOfMonth = new Date(calendar.day.getFullYear(), calendar.day.getMonth() + 1, 0);
+            calendar.currentMonth = getDaysFromMonth();
+            calendar.firstWeek = getFirstWeekFromMonth();
+            calendar.weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+            function getDaysFromMonth() {
+                var month = [];
+                for(var i = 0; i < calendar.lastDayOfMonth.getDate(); i++){
+                    month.push(new Date(calendar.firstDayOfMonth.getFullYear(), calendar.firstDayOfMonth.getMonth(), calendar.firstDayOfMonth.getDate() + i));
+                }
+                return month;
+            }
+
+            function getFirstWeekFromMonth() {
+                var firstWeek = [];
+                if(calendar.firstDayOfMonth.getDay() > 1) {
+                    for(var j = (calendar.firstDayOfMonth.getDay() - 2) * (-1); j <= 0; j++) {
+                        firstWeek.push(new Date(calendar.firstDayOfMonth.getFullYear(), calendar.firstDayOfMonth.getMonth(), j));
+                    }
+                }
+                for(var k = 0; k <= 7 - calendar.firstDayOfMonth.getDay(); k++) {
+                    firstWeek.push(new Date(calendar.firstDayOfMonth.getFullYear(), calendar.firstDayOfMonth.getMonth(), k + 1));
+                }
+                return firstWeek;
+            }
+        });
 })();
